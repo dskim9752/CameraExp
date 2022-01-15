4b1e565bf10e5ab42e9cd526afa21325080e1acd
Duplicat <dskim9752@gmail.com>
2022-01-15 12:59:05 +0900

    add plane color

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -18,12 +18,29 @@ public class CameraController : MonoBehaviour
     }
 
 
-
+/*
     private void LookAround()
     { 
         Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")); 
         Vector3 camAngle = cameraArm.rotation.eulerAngles; 
         cameraArm.rotation = Quaternion.Euler(camAngle.x - mouseDelta.y, camAngle.y + mouseDelta.x, camAngle.z); 
+    }
+*/
+
+    private void LookAround() 
+    { 
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        Vector3 camAngle = cameraArm.rotation.eulerAngles; //Ãß°¡ 
+        float x = camAngle.x - mouseDelta.y; 
+        if (x < 180f)
+        { 
+            x = Mathf.Clamp(x, -1f, 70f);
+        } 
+        else 
+        { 
+            x = Mathf.Clamp(x, 335f, 361f); 
+        } 
+        cameraArm.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z); 
     }
 
 }
